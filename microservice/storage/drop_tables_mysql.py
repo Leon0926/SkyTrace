be8a3e279559ae903204e27cd1254b7ad5c5e28a
Leon <leon.nl0926@gmail.com>
2026-03-02 20:43:05 -0800

    move microservice contents to root

--- microservice/storage/drop_tables_mysql.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-db_conn = mysql.connector.connect(host="lab6kafka.canadacentral.cloudapp.azure.com", user="user",
-password="ACIT3855", database="events")
-db_cursor = db_conn.cursor()
-db_cursor.execute('''
-DROP TABLE aircraft_location, aircraft_time_until_arrival''')
-db_conn.commit()
-db_conn.close()
